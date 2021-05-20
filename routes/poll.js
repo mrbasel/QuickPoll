0c79bb9f0576b7afefe624babd6568827767ff72
Basel <mrbasel555@gmail.com>
2021-05-20 17:46:19 +0300

    add form for creating polls

--- routes/poll.js
+++ routes/poll.js
@@ -1,9 +1,28 @@
 const express = require("express");
+
+const db = require("../db.js");
+
 const router = express.Router();
 
-/* GET home page. */
 router.get("/createPoll", function (req, res, next) {
   res.render("createPoll");
+});
+
+router.post("/createPoll", function (req, res, next) {
+  console.log(req.body);
+  const question = req.body.question;
+  const date = req.body.date;
+
+  delete req.body.question;
+  delete req.body.date;
+
+  const choices = Object.values(req.body);
+
+  db.Polls.createPoll(question)
+    .then((data) => console.log(data))
+    .catch((data) => console.log("ERROR: " + data));
+
+  res.redirect("/");
 });
 
 module.exports = router;
