8ba7365f537fddea874e8f1f404a9f21a913f5ec
Basel <mrbasel555@gmail.com>
2021-05-21 20:17:31 +0300

    add methods to db class

--- db.js
+++ db.js
@@ -9,7 +9,28 @@ class DatabaseWrapper {
 
 class Polls {
   static createPoll(questionText) {
-    return db.none("INSERT INTO polls(question) VALUES ($1)", questionText);
+    return db.one(
+      "INSERT INTO polls(question) VALUES ($1) RETURNING poll_id",
+      questionText
+    );
+  }
+  static getPoll(pollId) {
+    return db.one("SELECT * FROM polls WHERE url_id = $1", pollId);
+  }
+
+  static getChoices(pollId) {
+    return db.any("SELECT * FROM choices WHERE poll_id = $1", pollId);
+  }
+
+  static addChoices(pollId, choices) {
+    choices.map((choice) => {
+      return db.none(
+        "INSERT INTO choices (poll_id, choice_text) VALUES ($1, $2)",
+        [pollId, choice]
+      );
+    });
+
+    return Promise.all(choices);
   }
 }
 
