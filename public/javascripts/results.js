3dd7c183f575f4ca0aab03f303a68f8ce21eaeb0
Basel <mrbasel555@gmail.com>
2021-05-23 23:34:49 +0300

    add results page & route

---
+++ public/javascripts/results.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const urlId = window.location.pathname.split("/")[2];
+
+  fetch(`http://localhost:3000/poll/${urlId}/data`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let myChart = new Chart(ctx, {
+        type: "pie",
+        data: data,
+        options: {
+          responsive: false,
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    });
+};
--- routes/poll.js
+++ routes/poll.js
@@ -71,4 +71,56 @@ router.post("/:pollId", function (req, res, next) {
   res.redirect("/");
 });
 
+router.get("/:pollId/results", function (req, res, next) {
+  const urlId = req.url.slice(1).split("/")[0];
+
+  db.Polls.getPoll(urlId)
+    .then((poll) => {
+      res.render("results", {
+        question: poll.question,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(404);
+    });
+});
+
+router.get("/:pollId/data", function (req, res, next) {
+  const urlId = req.url.slice(1).split("/")[0];
+  db.Polls.getPoll(urlId)
+    .then((poll) => db.Polls.getChoices(poll.poll_id))
+    .then((choices) => {
+      console.log(choices);
+      const data = {
+        labels: choices.map((elem) => elem.choice_text),
+        datasets: [
+          {
+            label: "# of Votes",
+            data: choices.map((elem) => elem.vote_count),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      res.json(data);
+    });
+});
+
 module.exports = router;
