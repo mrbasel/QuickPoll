4283d47ffe2e63683ca7e8f205e47075edd88e74
Basel <mrbasel555@gmail.com>
2021-05-30 15:09:13 +0300

    refactor: use async/await syntax

--- routes/poll.js
+++ routes/poll.js
@@ -9,63 +9,54 @@ router.get("/create", function (req, res, next) {
   res.render("createPoll.html");
 });
 
-router.post("/create", function (req, res, next) {
+router.post("/create", async function (req, res, next) {
   const question = req.body.question;
   const date = req.body.date;
-
+  // Remove question and date so that only the choices
+  // will be remaining in the body
   delete req.body.question;
   delete req.body.date;
-
   const choices = Object.values(req.body);
 
-  let url_id;
-  db.Polls.createPoll(question, date + " 21:50:00")
-    .then((data) => {
-      url_id = data.url_id;
-      return db.Polls.addChoices(data.poll_id, choices);
-    })
-    .then(() => res.redirect("/poll/" + url_id))
-    .catch((data) => {
-      console.log("ERROR: " + data);
-      res.redirect("/");
-    });
+  try {
+    const pollData = await db.Polls.createPoll(question, date + " 21:50:00");
+    await db.Polls.addChoices(pollData.poll_id, choices);
+    res.redirect("/poll/" + pollData.url_id);
+  } catch (e) {
+    console.error(e);
+    res.redirect("/");
+  }
 });
 
-router.get("/:pollId", function (req, res, next) {
+router.get("/:pollId", async function (req, res, next) {
   const pollId = req.params.pollId;
-  let question;
 
-  db.Polls.getPoll(pollId)
-    .then((data) => {
-      question = data.question;
-      deadlineDate = data.deadline_date;
+  try {
+    const pollData = await db.Polls.getPoll(pollId);
+    if (new Date() > pollData.deadline_date)
+      throw new PollDeadlineError("Poll is already finished");
 
-      if (new Date() > deadlineDate)
-        throw new PollDeadlineError("Poll is finished");
+    const pollChoices = await db.Polls.getChoices(pollData.poll_id);
+    pollChoices.sort((a, b) => a.choice_id - b.choice_id);
+    let canVote = true;
 
-      return db.Polls.getChoices(data.poll_id);
-    })
-    .then((data) => {
-      data.sort((a, b) => a.choice_id - b.choice_id);
-      let canVote = true;
-      if (req.cookies[pollId] !== undefined) canVote = false;
+    if (req.cookies[pollId] !== undefined) canVote = false;
 
-      res.render("poll.html", {
-        question: question,
-        choices: data,
-        canVote: canVote,
-        pollId: pollId,
-      });
-    })
-    .catch((e) => {
-      console.log(e);
-      if (e instanceof PollDeadlineError)
-        res.redirect(req.originalUrl + "/results");
-      else res.redirect("/");
+    res.render("poll.html", {
+      question: pollData.question,
+      choices: pollChoices,
+      canVote: canVote,
+      pollId: pollId,
     });
+  } catch (e) {
+    console.log(e);
+    if (e instanceof PollDeadlineError)
+      res.redirect(req.originalUrl + "/results");
+    else res.redirect("/");
+  }
 });
 
-router.post("/:pollId", function (req, res, next) {
+router.post("/:pollId", async function (req, res, next) {
   const choice = req.body.choice;
   const urlId = req.url.slice(1);
 
@@ -73,68 +64,70 @@ router.post("/:pollId", function (req, res, next) {
     res.redirect(req.originalUrl + "/results");
   else {
     res.setHeader("set-cookie", `${urlId}=${choice}; Max-Age=86400`);
+    try {
+      const pollData = await db.Polls.getPoll(urlId);
+      await db.Polls.vote(pollData.poll_id, choice);
 
-    db.Polls.getPoll(urlId)
-      .then((poll) => {
-        return db.Polls.vote(poll.poll_id, choice);
-      })
-      .then(() => res.redirect(req.originalUrl + "/results"))
-      .catch((e) => {
-        res.redirect("/");
-        console.log("ERROR: " + e);
-      });
+      res.redirect(req.originalUrl + "/results");
+    } catch (e) {
+      res.redirect("/");
+      console.log("ERROR: " + e);
+    }
   }
 });
 
-router.get("/:pollId/results", function (req, res, next) {
+router.get("/:pollId/results", async function (req, res, next) {
   const urlId = req.url.slice(1).split("/")[0];
+  try {
+    const pollData = await db.Polls.getPoll(urlId);
 
-  db.Polls.getPoll(urlId)
-    .then((poll) => {
-      res.render("results.html", {
-        question: poll.question,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(404);
+    res.render("results.html", {
+      question: pollData.question,
     });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(404);
+  }
 });
 
-router.get("/:pollId/data", function (req, res, next) {
+router.get("/:pollId/data", async function (req, res, next) {
   const urlId = req.url.slice(1).split("/")[0];
-  db.Polls.getPoll(urlId)
-    .then((poll) => db.Polls.getChoices(poll.poll_id))
-    .then((choices) => {
-      const data = {
-        labels: choices.map((elem) => elem.choice_text),
-        datasets: [
-          {
-            label: "# of Votes",
-            data: choices.map((elem) => elem.vote_count),
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
 
-      res.json(data);
-    });
+  try {
+    const pollData = await db.Polls.getPoll(urlId);
+    const pollChoices = await db.Polls.getChoices(pollData.poll_id);
+
+    const data = {
+      labels: pollChoices.map((elem) => elem.choice_text),
+      datasets: [
+        {
+          label: "# of Votes",
+          data: pollChoices.map((elem) => elem.vote_count),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    res.json(data);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(404);
+  }
 });
 
 module.exports = router;
