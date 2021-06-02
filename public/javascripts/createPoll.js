25c97c1725fbee936098863df4dbc834d2d945e0
Basel <mrbasel555@gmail.com>
2021-06-03 00:49:06 +0300

    add frontend validation

--- public/javascripts/createPoll.js
+++ public/javascripts/createPoll.js
@@ -21,11 +21,45 @@ function textboxListener(elem) {
   answerTextFieldsContainer.appendChild(answerLabel);
   answerTextFieldsContainer.appendChild(answerTextbox);
 }
-window.onload = () => {
+
+function validAnswers() {
   const answerTextFields = document.querySelectorAll(".answer");
 
+  let validAnswersCount = 0;
+  for (const textField of answerTextFields) {
+    if (textField.value !== "") validAnswersCount++;
+  }
+
+  if (validAnswersCount < 2) return false;
+  return true;
+}
+
+// Sets the min attribute for the date input
+// to the current date, and sets max to a date
+// one year from the current date
+function setDateMinMax() {
+  const dateInput = document.querySelector("input[type='date']");
+  const dateAfterOneYear = new Date();
+  dateAfterOneYear.setFullYear(dateAfterOneYear.getFullYear() + 1);
+
+  dateInput.setAttribute("min", new Date().toISOString().split("T")[0]);
+  dateInput.setAttribute("max", dateAfterOneYear.toISOString().split("T")[0]);
+}
+
+window.onload = () => {
+  const submitBtn = document.querySelector("#submitBtn");
+  const answerTextFields = document.querySelectorAll(".answer");
+
+  setDateMinMax();
   answerTextFields[answerTextFields.length - 1].addEventListener(
     "focus",
     textboxListener
   );
+
+  submitBtn.addEventListener("click", (e) => {
+    if (!validAnswers()) {
+      alert("You must have 2 or more answers");
+      e.preventDefault();
+    }
+  });
 };
