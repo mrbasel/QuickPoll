778e6d020ed53eb17c39e1daaa3c58b6754dc124
Basel <mrbasel555@gmail.com>
2021-06-03 20:44:43 +0300

    use prepared statements to protect against sql injection

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const pgp = require("pg-promise")();
 const db = pgp(process.env.DATABASE_URL);
+const { PreparedStatement: PS } = require("pg-promise");
 
 class DbWrapper {
   static createPollsCmd = `CREATE TABLE IF NOT EXISTS polls (
@@ -28,35 +29,51 @@ class DbWrapper {
 
 class Polls {
   static createPoll(questionText, timeStamp) {
-    return db.one(
-      "INSERT INTO polls(question, deadline_date) VALUES ($1, $2) RETURNING poll_id, url_id",
-      [questionText, timeStamp]
-    );
+    const createPollStatement = new PS({
+      name: "create-poll",
+      text: "INSERT INTO polls(question, deadline_date) VALUES ($1, $2) RETURNING poll_id, url_id",
+      values: [questionText, timeStamp],
+    });
+    return db.one(createPollStatement);
   }
   static getPoll(pollId) {
-    return db.one("SELECT * FROM polls WHERE url_id = $1", pollId);
+    const getPollStatement = new PS({
+      name: "get-poll",
+      text: "SELECT * FROM polls WHERE url_id = $1",
+      values: [pollId],
+    });
+    return db.one(getPollStatement);
   }
 
   static getChoices(pollId) {
-    return db.any("SELECT * FROM choices WHERE poll_id = $1", pollId);
+    const getChoicesStatement = new PS({
+      name: "get-choices",
+      text: "SELECT * FROM choices WHERE poll_id = $1",
+      values: [pollId],
+    });
+    return db.any(getChoicesStatement);
   }
 
   static addChoices(pollId, choices) {
     choices.map((choice) => {
-      return db.none(
-        "INSERT INTO choices (poll_id, choice_text) VALUES ($1, $2)",
-        [pollId, choice]
-      );
+      const addChoiceStatement = new PS({
+        name: "add-choice",
+        text: "INSERT INTO choices (poll_id, choice_text) VALUES ($1, $2)",
+        values: [pollId, choice],
+      });
+      return db.none(addChoiceStatement);
     });
 
     return Promise.all(choices);
   }
 
   static vote(pollId, choice) {
-    return db.none(
-      "UPDATE choices SET vote_count = vote_count + 1 WHERE poll_id = $1 AND choice_text = $2 ",
-      [pollId, choice]
-    );
+    const voteStatement = new PS({
+      name: "vote",
+      text: "UPDATE choices SET vote_count = vote_count + 1 WHERE poll_id = $1 AND choice_text = $2",
+      values: [pollId, choice],
+    });
+    return db.none(voteStatement);
   }
 }
 
